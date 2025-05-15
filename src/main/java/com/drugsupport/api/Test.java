f7f92797dc8f094852efd43c35d3bdb2adec671d
Hikari_Coder <khanhkhoi08@gmail.com>
2025-05-15 09:56:26 +0700

    Update Project

---
+++ src/main/java/com/drugsupport/api/Test.java
@@ -0,0 +1,12 @@
+package com.drugsupport.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+	@GetMapping(value="/test")
+	public String getString() {
+		return "success KhanhTrinh";
+	}
+}
